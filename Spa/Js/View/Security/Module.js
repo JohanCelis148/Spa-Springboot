34962e246b4445da8d6ef0463da668d63647bf07
Kevinho1906 <kevinmurillobetancourt@gmail.com>
2023-05-24 17:02:47 -0500

    Actualizado

---
+++ Spa/Js/View/Security/Module.js
@@ -0,0 +1,114 @@
+//Cargar de manera automatica los datos regostrados
+// Busqueda por id
+function findById(id) {
+    $.ajax({
+        url: 'http://localhost:9000/backend-service/api/security/module/' + id,
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (item) {
+        $("#id").val(item.id)
+        $("#code").val(item.code)
+        $("#description").val(item.description)
+        $("#status").val(item.state==true?'1':'0')      
+    })
+}
+
+function loadTable() {
+    $.ajax({
+        url: 'http://localhost:9000/backend-service/api/security/module',
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (items) {
+        var registros = "";
+        items.forEach(function (item, index, array) {
+            registros += `
+
+                        <tr class="table-light">
+                            <td>`+item.code+`</td>
+                            <td>`+item.description+`</td>
+                            <td>`+(item.state==true?'Activo':'Inactivo')+`</td>
+                            <td><button class="btnEdit" type="button" onclick="findById(`+item.id+`);"><i class="fi fi-rr-pencil"></i></button></td>
+                            <td><button class="btnDelete" type="button" onclick="deleteById(`+item.id+`);"><i class="fi fi-rr-trash"></i></button></td>
+                        </tr>
+                        `;
+        })
+        $("#dataResult").html(registros);   
+    })
+}
+
+//Accion para eliminar un registro seleccionado 
+function deleteById(id){
+    $.ajax({
+        url: 'http://localhost:9000/backend-service/api/security/module/' + id,
+        method: "delete",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (result) {
+        loadTable();
+    })
+}
+
+
+//Accion de adicionar un registro
+function Add(){
+    $.ajax({
+        url: 'http://localhost:9000/backend-service/api/security/module',
+        data: JSON.stringify({
+            code: $("#code").val(),
+            description: $("#description").val(),
+            state: parseInt($("#status").val()),
+            userCreationId: 1,
+            dateCreation: new Date()
+        }),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (result) {
+        //Cargar datos
+        loadTable();
+
+        //Limpiar formulario
+        clearData();
+    })
+}
+
+
+//Accion de actualizar un registro
+function Update(){
+    $.ajax({
+        url: 'http://localhost:9000/backend-service/api/security/module/' + $("#id").val(),
+        data: JSON.stringify({
+            code: $("#code").val(),
+            description: $("#description").val(),
+            state: parseInt($("#status").val()),
+            userCreationId: 1,
+            dateCreation: new Date(),
+            userModificationId: 1,
+            dateModification: new Date()
+        }),
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).done(function (result) {
+        //Cargar datos
+        loadTable();
+
+        //Limpiar formulario
+        clearData();
+    })
+}
+
+// Función para limpiar datos
+function clearData(){
+    $("#id").val(""),
+    $("#code").val(""),
+    $("#description").val(""),
+    $("#status").val("")
+}
